70606059d63d7b709095cfe55d722697a253c723
Colin Ledbetter <colinmledbetter@gmail.com>
2015-04-03 20:34:40 -0400

    Fixed a bug where hitting the run button repeatedly would cause app to crash

--- RobotTurtlesApp/src/edu/bsu/cs222/cards/CardRunner.java
+++ RobotTurtlesApp/src/edu/bsu/cs222/cards/CardRunner.java
@@ -7,40 +7,37 @@ import edu.bsu.cs222.enums.Command;
 public class CardRunner {
 
 	private MapTileSetter mapTileSetter;
-	private Card card;
-	private Integer cardCounter = 0;
-	private CardList cardRunnerCardList;
+	private boolean running = false;
 
 	public CardRunner(MapTileSetter mapTileSetter) {
 		this.mapTileSetter = mapTileSetter;
 	}
 
 	public void runList(CardList cardList) {
-		this.cardRunnerCardList = cardList;
-		if (cardCounter < cardRunnerCardList.getSize()) {
-			runCardList();
-		}else{
-			mapTileSetter.updateScore();
+		System.out.println("runList hit");
+		if (running) {
+			return;
 		}
+		running = true;
+		runCardList(cardList);
+		mapTileSetter.updateScore();
+		running = false;
 	}
-	
-	private void runCardList(){
+
+	private void runCardList(final CardList cardList) {
+		System.out.println("runCardList hit");
 		Handler handler = new Handler();
 		handler.postDelayed(new Runnable() {
 			public void run() {
-				executeCardList();
+				for (Card card : cardList) {
+					runCard(card);
+				}
 			}
-		}, 500);
-	}
-	
-	private void executeCardList(){
-		card = cardRunnerCardList.getCard(cardCounter);
-		runCard(card);
-		cardCounter++;
-		runList(cardRunnerCardList);
+		}, 100);
 	}
 
 	private void runCard(Card card) {
+		System.out.println("runCard hit");
 		Command command = card.getCommand();
 		command.performTurtleAction(mapTileSetter);
 	}
--- RobotTurtlesApp/src/edu/bsu/cs222/enums/TileType.java
+++ RobotTurtlesApp/src/edu/bsu/cs222/enums/TileType.java
@@ -8,14 +8,13 @@ import edu.bsu.cs222.tiles.Tile;
 import edu.bsu.cs222.tiles.WoodBlockTile;
 
 public enum TileType {
-	
-	ICEBLOCK, JEWEL, OPENSPACE, PUDDLE, 
-	STONEBLOCK, WOODBLOCK, TURTLE;
-	
+
+	ICEBLOCK, JEWEL, OPENSPACE, PUDDLE, STONEBLOCK, WOODBLOCK, TURTLE;
+
 	private String string;
 	private Tile tile;
-	
-	static{
+
+	static {
 		ICEBLOCK.string = "iceBlockTiles";
 		JEWEL.string = "jewelTile";
 		OPENSPACE.string = "openSpaceTiles";
@@ -24,21 +23,25 @@ public enum TileType {
 		WOODBLOCK.string = "woodBlockTiles";
 		TURTLE.string = "turtleTile";
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return this.string;
 	}
-	
-	public Tile createTile(Location location){
-		switch (this){
-		case OPENSPACE: tile = new OpenSpaceTile(location);
-						break;
-		case STONEBLOCK: tile = new StoneBlockTile(location);
-						break;
-		case WOODBLOCK: tile = new WoodBlockTile(location);
-						break;
-		case ICEBLOCK: tile = new IceBlockTile(location);
-						break;
+
+	public Tile createTile(Location location) {
+		switch (this) {
+		case OPENSPACE:
+			tile = new OpenSpaceTile(location);
+			break;
+		case STONEBLOCK:
+			tile = new StoneBlockTile(location);
+			break;
+		case WOODBLOCK:
+			tile = new WoodBlockTile(location);
+			break;
+		case ICEBLOCK:
+			tile = new IceBlockTile(location);
+			break;
 		default:
 			break;
 		}
