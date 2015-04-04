05410baf2e17e72f7438d3a198fe3d22a3d6e5a1
Colin Ledbetter <colinmledbetter@gmail.com>
2015-04-03 20:18:01 -0400

    Algorithm implemented for easy mazes, constructor needs to be made

--- RobotTurtlesApp/src/edu/bsu/cs222/TurtleBot.java
+++ RobotTurtlesApp/src/edu/bsu/cs222/TurtleBot.java
@@ -10,15 +10,20 @@ import edu.bsu.cs222.tiles.OpenSpaceTile;
 import edu.bsu.cs222.tiles.Tile;
 
 public class TurtleBot {
+
+	private static final int WEST_BLOCK_MODIFIER = -1;
+	private static final int EAST_BLOCK_MODIFIER = 1;
+	private static final int NORTH_BLOCK_MODIFIER = -8;
+	private static final int SOUTH_BLOCK_MODIFIER = 8;
 	
 	private Stack<Tile> visited = new Stack<Tile>();
 	private List<Tile> deadEnds = new ArrayList<Tile>();
 	private TurtleMover mover;
 	private GameMap map;
 	private int leftCount = 0;
-	
+
 	public TurtleBot() {
-		
+
 	}
 
 	public void go() {
@@ -28,9 +33,12 @@ public class TurtleBot {
 			visited.push(map.getTile(lastLocation));
 			leftCount = 0;
 		} else {
-			mover.getTile().turnTurtleLeft(); leftCount++;
-			if (mover.getTile().getNextTile(mover.getTile().getDirection()).getClass().equals(OpenSpaceTile.class)) {
-				mover.getTile().turnTurtleLeft(); leftCount++;
+			mover.getTile().turnTurtleLeft();
+			leftCount++;
+			if (mover.getTile().getNextTile(mover.getTile().getDirection())
+					.getClass().equals(OpenSpaceTile.class)) {
+				mover.getTile().turnTurtleLeft();
+				leftCount++;
 				go();
 			} else {
 				if (leftCount == 4) {
@@ -41,23 +49,44 @@ public class TurtleBot {
 			}
 		}
 	}
-	
+
 	public void deadEnd() {
 		Location deadLocation = mover.getTile().getLocation();
-		visited.peek();
+		moveToLastVisitedTile();
+		deadEnds.add(map.getTile(deadLocation));
+		go();
 	}
-	
-	private Direction getDirectionToLastVisitedTile() {
-		if (visited.peek().getLocation().getTileLocation() == mover.getTile().getLocation().getTileLocation() - 1) { //West
-			
-		} else if (visited.peek().getLocation().getTileLocation() == mover.getTile().getLocation().getTileLocation() + 1) { //East
-			
-		} else if (visited.peek().getLocation().getTileLocation() == mover.getTile().getLocation().getTileLocation() - 8) { //North
-			
-		} else { //South
-			
+
+	// Turtle turns left until it faces the last visited tile and moves forward
+	private void moveToLastVisitedTile() {
+		// West
+		if (visited.peek().getLocation().getTileLocation() == mover.getTile()
+				.getLocation().getTileLocation() - WEST_BLOCK_MODIFIER) {
+			while (mover.getTile().getDirection() != Direction.WEST) {
+				mover.getTile().turnTurtleLeft();
+			}
 		}
-		return null;
+		// East
+		else if (visited.peek().getLocation().getTileLocation() == mover
+				.getTile().getLocation().getTileLocation() + EAST_BLOCK_MODIFIER) {
+			while (mover.getTile().getDirection() != Direction.EAST) {
+				mover.getTile().turnTurtleLeft();
+			}
+		}
+		// North
+		else if (visited.peek().getLocation().getTileLocation() == mover
+				.getTile().getLocation().getTileLocation() - NORTH_BLOCK_MODIFIER) {
+			while (mover.getTile().getDirection() != Direction.NORTH) {
+				mover.getTile().turnTurtleLeft();
+			}
+		}
+		// South
+		else if (visited.peek().getLocation().getTileLocation() == mover
+				.getTile().getLocation().getTileLocation() - SOUTH_BLOCK_MODIFIER){
+			while (mover.getTile().getDirection() != Direction.SOUTH) {
+				mover.getTile().turnTurtleLeft();
+			}
+		}
 	}
 
 }
